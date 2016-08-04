53d9901a1bd2b5819a444bd162baf681107e149d
snehesht <mail@snehesh.me>
2016-08-04 15:17:12 -0400

    letsencrypt auto script

---
+++ letsencrypt/create_ssl_certs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Install Certbot (certbot.eff.org)
+wget https://dl.eff.org/certbot-auto
+chmod a+x certbot-auto
+
+# Shutdown Nginx
+sudo service nginx stop
+
+# Create new certificates
+export TMPCMD="certonly --standalone"
+
+# iterate through args
+for var in "$@"
+do
+	export TMPCMD=$TMPCMD" -d ""$var"
+done
+echo $TMPCMD
+
+# Fetch the certificates
+sudo ./certbot-auto certonly $TMPCMD
+
+# Creating dir in /etc/nginx
+sudo mkdir -p /etc/nginx/certs
+
+# linking certs from /etc/letsencrypt/live/domainname.com 
+sudo ln -s /etc/letsencrypt/live/* /etc/nginx/certs/
+
+# next steps 
+#echo "Execute the following commands" 
+#echo ""
+#echo "openssl dhparam -out /etc/nginx/certs/DOMAIN_NAME_HERE/dhparam.pem 2048"
+
